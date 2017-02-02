a3e3d02a2112160c8bc4e0fe2ffa85c113addf0e
ezio84 <brabus84@gmail.com>
2017-02-03 00:26:51 +0100

    Settings: Allow to quickly open Running Services from meminfo view
    @neobuddy89: Updated for A14.
    
    Change-Id: I252ce85bda3daab5c5e4db097a6fd743365bc309
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -41,6 +41,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appcompat.UserAspectRatioDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
@@ -385,7 +386,8 @@ public class SettingsGateway {
             LongBackgroundTasksDetails.class.getName(),
             RegionalPreferencesEntriesFragment.class.getName(),
             BatteryInfoFragment.class.getName(),
-            UserAspectRatioDetails.class.getName()
+            UserAspectRatioDetails.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -432,5 +434,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }
